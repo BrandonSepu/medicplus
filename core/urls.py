0dd33549cb6698ffad53c464417e508891bbd084
BrandonSepu <bra.sepulvedam@duocuc.cl>
2021-11-22 14:16:50 -0300

    contact funcionando

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("base", views.base, name="base"),
     path("", views.index, name="index"),
     path("tomahora", views.tomahora, name="tomahora"),
+    path("contact", views.contact, name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import contactForm
 
 # Create your views here.
 
@@ -10,4 +11,22 @@ def index(request):
 
 def tomahora(request):
     return render(request, 'web/tomahora.html')
+
+def contact(request): #CONTACTO
+
+    msnform = contactForm()
+    data = {'cform' : msnform}
+    
+    if request.method == 'POST':
+        msnform = contactForm(data = request.POST) 
+        if msnform.is_valid():
+            msnform.save()
+        else:
+            data["cform"] = msnform;
+        
+        print("Mensaje enviado Correctamente")
+    else:
+        print("No se puedo enviar el mensaje, revisa los datos")
+        
+    return render(request, 'web/contact.html',  data)
 
